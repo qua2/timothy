ad3ac0b41ea17848092f367e695fbcf453e3b5bb
qua2 <86092745+qua2@users.noreply.github.com>
2021-12-20 12:23:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,37 @@ def welcome():
     return 'this is flask file example'
 if __name__=="__main__":
     app.run(host = "127.0.0.1",port=8080, debug=True)
+### Building Url Dynamically 
+####Variable Rules And URL Building
+
+from flask import Flask,redirect,url_for
+
+app=Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to Flask world22222'
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "<html><body><h1>The Reult is passed and the mark is </h1></body></html>" + str(score)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The Person has failed and the marks is "+ str(score)
+
+### Result checker
+@app.route('/results/<int:marks>')
+def results(marks):
+    result =" "
+    if marks<50:
+        result='fail'
+    else:
+        result='success'
+    #return result  (initate this first )
+    return redirect(url_for(result,score=marks)) #(initiate this next)
+
+
+if __name__=='__main__':
+    app.run(debug=True)    
